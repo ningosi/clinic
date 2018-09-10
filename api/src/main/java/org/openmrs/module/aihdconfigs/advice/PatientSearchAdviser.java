2ff5936d73cd0270d638973a4b53ddb5101c2ca4
ningosi <nicotwendelee@gmail.com>
2018-09-10 17:45:40 +0300

    Proper searching based on patient identifer location as oppossed to ealier person attribute

--- api/src/main/java/org/openmrs/module/aihdconfigs/advice/PatientSearchAdviser.java
+++ api/src/main/java/org/openmrs/module/aihdconfigs/advice/PatientSearchAdviser.java
@@ -3,16 +3,12 @@ package org.openmrs.module.aihdconfigs.advice;
 import org.aopalliance.aop.Advice;
 import org.aopalliance.intercept.MethodInterceptor;
 import org.aopalliance.intercept.MethodInvocation;
-import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.openmrs.Location;
 import org.openmrs.Patient;
-import org.openmrs.PersonAttribute;
-import org.openmrs.PersonAttributeType;
+import org.openmrs.PatientIdentifier;
 import org.openmrs.api.context.Context;
 import org.openmrs.api.context.Daemon;
-import org.openmrs.module.aihdconfigs.metadata.PersonAttributeTypes;
 import org.springframework.aop.Advisor;
 import org.springframework.aop.support.StaticMethodMatcherPointcutAdvisor;
 
@@ -55,22 +51,19 @@ public class PatientSearchAdviser extends StaticMethodMatcherPointcutAdvisor imp
             }
 
             Integer sessionLocationId = Context.getUserContext().getLocationId();
-            String locationAttributeUuid = PersonAttributeTypes.PATIENT_LOCATION.uuid();
-            if (StringUtils.isNotBlank(locationAttributeUuid)) {
-                final PersonAttributeType personAttributeType = Context.getPersonService().getPersonAttributeTypeByUuid(locationAttributeUuid);
                 if (sessionLocationId != null) {
                     String sessionLocationUuid = Context.getLocationService().getLocation(sessionLocationId).getUuid();
                     if(object instanceof List) {
                         List<Patient> patientList = (List<Patient>) object;
                         for (Iterator<Patient> iterator = patientList.iterator(); iterator.hasNext(); ) {
-                            if(!doesPatientBelongToGivenLocation(iterator.next(), personAttributeType, sessionLocationUuid)) {
+                            if(!doesPatientBelongToGivenLocation(iterator.next(), sessionLocationUuid)) {
                                 iterator.remove();
                             }
                         }
                         object = patientList;
                     }
                     else if(object instanceof Patient) {
-                        if(!doesPatientBelongToGivenLocation((Patient)object, personAttributeType, sessionLocationUuid)) {
+                        if(!doesPatientBelongToGivenLocation((Patient)object, sessionLocationUuid)) {
                             object = null;
                         }
                     }
@@ -85,30 +78,18 @@ public class PatientSearchAdviser extends StaticMethodMatcherPointcutAdvisor imp
                         return new ArrayList<Patient>();
                     }
                 }
-            }
             return object;
         }
 
-        private Boolean doesPatientBelongToGivenLocation(Patient patient, PersonAttributeType personAttributeType, String sessionLocationUuid) {
-            PersonAttribute personAttribute = patient.getAttribute(personAttributeType);
-            return (personAttribute != null && compare(personAttribute.getValue(), sessionLocationUuid));
-        }
-
-        private Boolean compare(String value1, String value2) {
-            boolean isSame = false;
-            Location loc1 = getMyLocation(value1);
-            Location loc2 = Context.getLocationService().getLocationByUuid(value2);
-            if(loc1 != null && loc2 != null && loc1.equals(loc2)){
-                isSame = true;
+        private boolean doesPatientBelongToGivenLocation(Patient patient, String sessionLocationUuid) {
+            boolean thereIsMatch = false;
+            PatientIdentifier patientIdentifier = patient.getPatientIdentifier(Context.getPatientService().getPatientIdentifierTypeByUuid("b9ba3418-7108-450c-bcff-7bc1ed5c42d1"));
+            if(patientIdentifier != null && patientIdentifier.getLocation() != null) {
+                if(patientIdentifier.getLocation().equals(Context.getLocationService().getLocationByUuid(sessionLocationUuid))){
+                    thereIsMatch = true;
+                }
             }
-            return  isSame;
-        }
-
-        private String correctValue(String str){
-            return str.replace("_", " ");
-        }
-        private Location getMyLocation(String name){
-            return Context.getLocationService().getLocation(correctValue(name));
+            return thereIsMatch;
         }
     }
 }
